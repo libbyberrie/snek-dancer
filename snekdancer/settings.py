26e71f2f626316e6870e1071a6e814d436ff924b
Merrin <merrin.macleod@gmail.com>
2019-08-05 15:07:16 +1000

    allow local host in the settings

--- snekdancer/settings.py
+++ snekdancer/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '=8qxnrur_k78xy_9%ce1!t+ejg*##91lvdry02=)(fb2&p3-9e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -119,4 +118,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
