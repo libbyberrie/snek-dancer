6156b9b5f4874d83f4a0eb3ac4704ffba8a4d637
Libby Berrie <libbyberrie@me.com>
2019-08-05 15:34:44 +1000

    Create an API view based off generic REST framework, using serialiser content

--- farms/views.py
+++ farms/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from farms.models import Farm
+from farms.serializers import FarmSerializer
+from rest_framework import generics
+
+class FarmListCreate(generics.ListCreateAPIView):
+	queryset = Farm.objects.all()
+	serializer_class = FarmSerializer
 
 # Create your views here.
