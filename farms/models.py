e4e91a51a4b16d29f8adb33a6ebdacbc8fa9679e
Libby Berrie <libbyberrie@me.com>
2019-08-06 11:44:10 +1000

    Change label fields for farm model

--- farms/models.py
+++ farms/models.py
@@ -4,5 +4,5 @@ class Farm(models.Model):
 	name = models.CharField(max_length=255)
 	modified = models.DateTimeField(auto_now_add=True)
 	micropythons = models.IntegerField()
-	minipythons = models.IntegerField()
+	curlyboi_snektions = models.IntegerField()
 
