04fcc7f7f7841584047dafeb6fe59a632f947d83
Merrin Macleod <merrin.macleod@gmail.com>
2019-08-06 13:38:03 +1000

    Hook line and snek (#13)
    * Change label fields for farm model
    
    * reflect changed field in farm model
    
    * Create farm component which receives data to collect and grow curlybois. Print name.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
 import Snek from "./Snek";
+import Farm from "./Farm";
 const App = () => (
-  <div>
-    <DataProvider endpoint="api/farm"
-                  render={data => <Table data={data} />} />
-    <Snek size={1} length={5} />
-    <Snek size={1} length={5} />
-    <Snek size={1} length={5} />
-    <Snek size={1} length={5} />
-    <Snek size={3} length={20} />
-  </div>
+  <DataProvider endpoint="api/farm" 
+                render={data => <div>
+                	<Farm farm={data[0]}/>
+                	<Table data={data} />
+            	    <Snek size={1} length={5} />
+				    <Snek size={1} length={5} />
+				    <Snek size={1} length={5} />
+				    <Snek size={1} length={5} />
+				    <Snek size={3} length={20} />
+            	</div>} />
 );
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
