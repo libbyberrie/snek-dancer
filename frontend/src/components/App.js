414deab99cbbad1fb1b9775daf827d4ad8765dbc
Merrin <merrin.macleod@gmail.com>
2019-08-06 13:45:43 +1000

    show curlyboi sections and micropythons

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,15 +5,10 @@ import Table from "./Table";
 import Snek from "./Snek";
 import Farm from "./Farm";
 const App = () => (
-  <DataProvider endpoint="api/farm" 
+  <DataProvider endpoint="api/farm"
                 render={data => <div>
                 	<Farm farm={data[0]}/>
                 	<Table data={data} />
-            	    <Snek size={1} length={5} />
-				    <Snek size={1} length={5} />
-				    <Snek size={1} length={5} />
-				    <Snek size={1} length={5} />
-				    <Snek size={3} length={20} />
             	</div>} />
 );
 const wrapper = document.getElementById("app");
