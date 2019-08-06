aa9f0414d2cc3a2035bef8b13e89b5e4c44c45ab
Libby Berrie <libbyberrie@me.com>
2019-08-06 13:54:13 +1000

    snek button

--- frontend/src/components/Farm.js
+++ frontend/src/components/Farm.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Farm = (props) => {
-  console.log("Data", props.farm);
+  const [state, setState] = useState(props.farm) 
+  const {micropythons, curlyboi_snektions} = state
+  console.log("Data", state);
+  const updateSneks = () => {
+    setState(
+        {...state, micropythons: state.micropythons += 1}
+      )
+    console.log("New state: ", state)
+  }
 
-  return <h1>Welcome to {props.farm.name} farm</h1>
+  return <div>
+            <h1>Welcome to {props.farm.name} farm</h1>
+            <p>You have collected {micropythons} micropythons</p>
+            <button onClick={updateSneks}>Moar sneks, please!</button>
+          </div>
+  
 }
 
 export default Farm;
