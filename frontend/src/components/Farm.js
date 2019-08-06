34a1fba68e84f7a7c1edb4babd50f26e43aff2a7
Libby Berrie <libbyberrie@me.com>
2019-08-06 14:02:58 +1000

    merge merrin's snekgenerator

--- frontend/src/components/Farm.js
+++ frontend/src/components/Farm.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Snek from "./Snek";
 
 const Farm = (props) => {
-  console.log("Data", props.farm);
+  const [state, setState] = useState(props.farm) 
+  const {micropythons, curlyboi_snektions} = state
+  console.log("Data", state);
+  const updateSneks = () => {
+    setState(
+        {...state, micropythons: state.micropythons += 1}
+      )
+    console.log("New state: ", state)
+  }
+  console.log("Data", state);
   let micropython_array = [];
   let curlyboi = null;
-  for (var i = 0; i < props.farm.micropythons; i++) {
+  for (var i = 0; i < micropythons; i++) {
     micropython_array.push(<Snek size={1} length={5} />)
   }
 
-  if(props.farm.curlyboi_snektions > 0) {
-    curlyboi = <Snek size={3} length={props.farm.curlyboi_snektions} />
+  if(state.curlyboi_snektions > 0) {
+    curlyboi = <Snek size={3} length={curlyboi_snektions} />
   }
 
   return(
     <div>
-      <h1>Welcome to {props.farm.name} farm</h1>
-      <p>You have collected {micropythons} micropythons</p>
-      <button onClick={updateSneks}>Moar sneks, please!</button>
+      <div>
+        <h1>Welcome to your farm</h1>
+        <p>You have collected {micropythons} micropythons</p>
+        <button onClick={updateSneks}>Moar sneks, please!</button>
+      </div>      
       <h2>Micropythons</h2>
       { micropython_array }
       <h2>Curlyboi</h2>
