e2727e7ec1db729ca8ca3a68ddce62deb66f773e
Drew Noll <drew@myxplor.com>
2019-08-08 16:47:14 +1000

    :fire: Bye Bye Table We Will Miss U

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
-import Table from "./Table";
 import Snek from "./Snek";
 import Farm from "./Farm";
 const App = () => (
-  <DataProvider endpoint="api/farm"
-                render={data => <div>
-                	<Farm farm={data[0]}/>
-                	<Table data={data} />
-            	</div>} />
+	<DataProvider
+	  endpoint="api/farm"
+    render={data => <Farm farm={data[0]}/>}
+	/>
 );
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
