80e3538c7d8d7c3616585c5ba7b7354544fb5f5c
Drew Noll <drew@myxplor.com>
2019-08-20 21:17:45 +1000

    :wrench: Fix The Animations! Add A Bigger Button! Do It All In One Big Stupid Commit!
    Sorry frens

--- frontend/src/components/Farm.js
+++ frontend/src/components/Farm.js
@@ -3,6 +3,7 @@ import Snek from "./Snek/";
 
 const Farm = ({ farm }) => {
   const initialState = {
+    name: null,
     micropythons: 0,
     curlyboi_snektions: 0,
     up: true
@@ -11,10 +12,11 @@ const Farm = ({ farm }) => {
   const [state, setState] = useState(initialState)
   const {name, micropythons, curlyboi_snektions, up} = state
 
+  // Call 'setState' only on initial component mount,
+  // with the passed in 'farm', if it exists
   useEffect(() => {
     farm ? setState(farm) : null
-    }, []
-  )
+  }, [])
 
   // Log out the state when it changes
   useEffect(() => {
@@ -23,61 +25,55 @@ const Farm = ({ farm }) => {
   )
 
   const updateSneks = () => {
-    let micros = state.micropythons;
-    let boi = state.curlyboi_snektions;
-    let uppy = state.up;
-      if (micros < 10 ) {
-        micros += 1;
-        uppy = !uppy;
-      } else  {
-        micros = 0;
-        boi += 1;
-        uppy = !uppy;
-      }
-      setState(
-        { ...state, micropythons: micros, curlyboi_snektions: boi, up: uppy }
-      )
-    console.log("New state: ", state)
-  }
+    let micros = micropythons;
+    let boi = curlyboi_snektions;
+    let uppy = up;
 
-
-  const Micropythons = () => {
-    let micropys = [];
-    for (var i = 0; i < micropythons; i++) {
-      micropys.push(<Snek key={i} size="medium" length={5} up={state.up}/>)
+    if (micros < 10 ) {
+      micros += 1;
+      uppy = !uppy;
+    } else  {
+      micros = 0;
+      boi += 1;
+      uppy = !uppy;
     }
-    return(
-      <div className="micropythons">
-        {micropys}
-      </div>
+    setState(
+      { ...state, micropythons: micros, curlyboi_snektions: boi, up: uppy }
     )
   }
 
-  const Curlyboi = () => (
-    curlyboi_snektions > 0
-    ? <Snek size="curlyboi" length={curlyboi_snektions} up={state.up}/>
-    : null
-  )
+  const micropys = (
+    [...Array(micropythons)].map((_, i) =>
+      (<Snek key={i} size="medium" length={5} up={i%2===0 ? up : !up}/>))
+  );
 
   return(
     <div>
       <header>
         <h1>Welcome to {name} Farm</h1>
-        { curlyboi_snektions > 0 && <p>You have cultivated a curlyboi that is {curlyboi_snektions} snektions long. That's a solid effort!</p>}
       </header>
       <div className="farm">
         <section className="farm-section farm-buttons">
-          <button onClick={updateSneks}>Moar sneks, please!</button>
+          <button className="btn-large" onClick={updateSneks}><h2>Moar sneks, please!</h2></button>
         </section>
 
         <section className="farm-section farm-long-grass">
-          <Micropythons/>
-          <Curlyboi/>
+
+          <div className="micropythons">
+            {micropys}
+          </div>
+
+          {curlyboi_snektions > 0 &&
+            <div className="the-curlyboi">
+              {<Snek size="curlyboi" length={curlyboi_snektions} up={up}/>}
+            </div>
+          }
         </section>
 
         <section className="farm-section farm-snek-board">
           <p>Micropythons: {micropythons}</p>
           <p>Curlyboi Length: {curlyboi_snektions}</p>
+          { curlyboi_snektions > 0 && <p>You have cultivated a curlyboi that is {curlyboi_snektions} snektions long. That's a solid effort!</p>}
         </section>
       </div>
     </div>
